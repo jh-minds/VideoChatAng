fea2dc4bda59d37629434ec39edd045ca4826b97
Jeppe Helholm <jeppe.helholm@twoday.com>
2025-02-04 09:00:15 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { VideoCallComponent } from './features/video-chat/components/video-call.component';
+import { VideoChatComponent } from './features/video-chat/components/video-call.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, VideoCallComponent],
+  imports: [RouterOutlet, VideoChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
